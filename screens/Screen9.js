decf828b1b9b6f75b4727fbe3ed5b2717a14a267
Abhijeet Singh Rathour <uic.18mca8021@gmail.com>
2022-01-12 19:37:11 +0530

    Screen9, Screen10 and Date is modified

--- screens/Screen9.js
+++ screens/Screen9.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Image, StyleSheet, Text, TextInput,TouchableOpacity,SafeAreaView,ScrollView,Modal} from 'react-native';
+import { View, Image, StyleSheet, Text,TouchableOpacity,SafeAreaView,Modal} from 'react-native';
 import Date from './Date';
 import {useState} from 'react';
 import { StylesContext } from '@material-ui/styles';
@@ -18,7 +18,8 @@ const Screen9 = ({navigation}) => {
         <SafeAreaView >
         <View>
           <View style={styles.bg1}>
-            <Image style={styles.direct} source={require('../images/back.png')} onPress={() => navigation.navigate('Screen4')}/>
+            <Image style={styles.direct} source={require('../images/back.png')} 
+            onPress={() => navigation.navigate('Screen4')}/>
             <Text style={styles.txt1}
             onPress={() => navigation.navigate('Screen10')}> Cleaning Control </Text>
             <Separator/>
@@ -36,8 +37,8 @@ const Screen9 = ({navigation}) => {
           <Text style={styles.txtcost1}>$ 39</Text>
           
           
-          <Text style={styles.txt6}>Chain Lubrication</Text>
-          <Image style={styles.imgclock1} source={require('../images/clock.png')}/>
+          <Text style={styles.txt3}>Chain Lubrication</Text>
+          <Image style={styles.imgclock2} source={require('../images/clock.png')}/>
           <Text style={styles.txttime2}> 20 min</Text>
           <Text style={styles.txtmore2}>More info</Text>
           <Text style={styles.txtcost2}>$ 39</Text>
@@ -90,34 +91,34 @@ const Screen9 = ({navigation}) => {
 
 
         <Text style={styles.txtbig2}>Afternoon</Text>        
-        <View style={{alignSelf:'center',height:5,width:50,left:-107,bottom:280,borderWidth:1,borderColor:'grey',paddingBottom:30,top:-785,borderRadius:7}} ></View>
-        <View style={{alignSelf:'center',height:5,width:50,left:-47,bottom:280,borderWidth:1,borderColor:'grey',paddingBottom:30,top:-817,borderRadius:7}}></View>
-        <View style={{alignSelf:'center',height:5,width:50,left:13,bottom:280,borderWidth:1,borderColor:'grey',paddingBottom:30,top:-849,borderRadius:7}}></View>
-        <View style={{alignSelf:'center',height:5,width:50,left:73,bottom:280,borderWidth:1,borderColor:'grey',paddingBottom:30,top:-881,borderRadius:7}}></View>
+        <View style={styles.view4} ></View>
+        <View style={styles.view5}></View>
+        <View style={styles.view6}></View>
+        <View style={styles.view7}></View>
         <Text style={styles.txtsmall4}>01:00</Text>
         <Text style={styles.txtsmall5}>01:45</Text>
         <Text style={styles.txtsmall6}>02:30</Text>
         <Text style={styles.txtsmall7}>03:15</Text>
 
-        <View style={{alignSelf:'center',height:5,width:50,left:-107,bottom:280,borderWidth:1,borderColor:'grey',paddingBottom:30,top:-875,borderRadius:7}}></View>
-        <View style={{alignSelf:'center',height:5,width:50,left:-47,bottom:280,borderWidth:1,borderColor:'grey',paddingBottom:30,top:-907,borderRadius:7}}></View>
-        <View style={{alignSelf:'center',height:5,width:50,left:13,bottom:280,borderWidth:1,borderColor:'grey',paddingBottom:30,top:-939,borderRadius:7}}></View>
-        <View style={{alignSelf:'center',height:5,width:50,left:73,bottom:280,borderWidth:1,borderColor:'grey',paddingBottom:30,top:-971,borderRadius:7}}></View>
+        <View style={styles.view8}></View>
+        <View style={styles.view9}></View>
+        <View style={styles.view10}></View>
+        <View style={styles.view11}></View>
         <Text style={styles.txtsmall8}>04:00</Text>
         <Text style={styles.txtsmall9}>04:45</Text>
         <Text style={styles.txtsmall10}>05:30</Text>
         <Text style={styles.txtsmall11}>06:15</Text>
 
-        <View style={{alignSelf:'center',height:5,width:50,left:-107,bottom:280,borderWidth:1,borderColor:'grey',paddingBottom:30,top:-965,borderRadius:7}}></View>
-        <View style={{alignSelf:'center',height:5,width:50,left:-47,bottom:280,borderWidth:1,borderColor:'grey',paddingBottom:30,top:-997,borderRadius:7}}></View>
-        <View style={{alignSelf:'center',height:5,width:50,left:13,bottom:280,borderWidth:1,borderColor:'grey',paddingBottom:30,top:-1029,borderRadius:7}}></View>
-        <Text style={{textTransform: 'capitalize', fontSize: 14,color: 'black',fontWeight: 'normal',top:481,position: 'absolute',left:53}}>07:00</Text>
-        <Text style={{textTransform: 'capitalize', fontSize: 14,color: 'black',fontWeight: 'normal',top:481,position: 'absolute',left:113}}>07:45</Text>
-        <Text style={{textTransform: 'capitalize', fontSize: 14,color: 'black',fontWeight: 'normal',top:481,position: 'absolute',left:173}}>08:30</Text>
+        <View style={styles.view12}></View>
+        <View style={styles.view13}></View>
+        <View style={styles.view14}></View>
+        <Text style={styles.txtsmall12}>07:00</Text>
+        <Text style={styles.txtsmall13}>07:45</Text>
+        <Text style={styles.txtsmall14}>08:30</Text>
 
-        <Image source={require('../images/sidellipse.png')} style={{ position: 'absolute', margin: 10, marginStart: 23,top:300,height:10,width:10 }}></Image>
-        <Image source={require('../images/verticaline.png')} style={{ position: 'absolute', marginStart: 26.5,top:319, }}></Image>
-        <Image source={require('../images/sidellipse.png')} style={{ position: 'absolute', margin: 10, marginStart: 23,top:368,height:10,width:10 }}></Image>
+        <Image source={require('../images/sidellipse.png')} style={styles.imgline1}></Image>
+        <Image source={require('../images/verticaline.png')} style={styles.imgline2}></Image>
+        <Image source={require('../images/sidellipse.png')} style={styles.imgellipse}></Image>
 
         <Image source={require('../images/separator2.png')} style={styles.sep1}></Image>
         <Image source={require('../images/separator2.png')} style={styles.sep2}></Image>
@@ -165,60 +166,21 @@ const Screen9 = ({navigation}) => {
         </SafeAreaView>
         </View>
 
-        <SafeAreaView style={{flex:0}}>        
+        <SafeAreaView style={styles.safearea}>        
        <View style={StylesContext.container}>
-           <Modal
-            animationType={'slide'}
-            transparent={false}
-            visible={showModal}
-            onRequestClose={() =>{
-                console.log('Modal has been closed.');
-            }}>
+           <Modal animationType={'slide'} transparent={false} visible={showModal}
+                  onRequestClose={() =>{ console.log('Modal has been closed.');}}>
             <View style={styles.modal}>
-            <Text style={{alignSelf:'center',color:'black',fontSize:18,bottom:-80,fontWeight:'800'}}>Confirmation </Text>
-                <Text style={{bottom:-120,alignSelf:'center',color:'black', fontSize:15,fontWeight:'500'}}>General Servicing</Text>
-                <Text style={{bottom:-120,alignSelf:'center',color:'grey', fontSize:15}}>of Harley Davidson SQ 12345</Text>
-                <Text style={{bottom:-120,alignSelf:'center',color:'black', fontSize:15,left:10,fontWeight:'500'}}>11:30 AM, 19th Sep2021.</Text>
-                <Text style={{bottom:-100,alignSelf:'center',color:'grey', fontSize:15,right:83}}>at</Text>
-                <TouchableOpacity style={{elevation:8,
-      backgroundColor:"#f2994a",
-      borderRadius:15,
-      paddingVertical:30,
-      paddingHorizontal:12,
-      margin: 20,
-      width:280,
-      left:20,
-      top:80,
-      marginTop:60,
-      marginBottom:60,
-      paddingBottom:5}} onPress={() => navigation.navigate('Screen10')} >
-              <Text style={{fontSize:18,
-        color:"white",
-        fontWeight:"bold",
-        alignSelf:"center",
-        textTransform:'capitalize',
-        bottom:15}}>Continue</Text>
+            <Text style={styles.txtmodal1}>Confirmation </Text>
+                <Text style={styles.txtmodal2}>General Servicing</Text>
+                <Text style={styles.txtmodal3}>of Harley Davidson SQ 12345</Text>
+                <Text style={styles.txtmodal4}>11:30 AM, 19th Sep2021.</Text>
+                <Text style={styles.txtmodal5}>at</Text>
+                <TouchableOpacity style={styles.to1} onPress={() => navigation.navigate('Screen10')} >
+              <Text style={styles.txtmodal6}>Continue</Text>
               </TouchableOpacity>
-
-              <TouchableOpacity style={{elevation:8,
-      backgroundColor:"#f0f0f0",
-      borderRadius:15,
-      paddingVertical:20,
-      paddingHorizontal:12,
-      margin: 20,
-      width:280,
-      left:20,
-      top:-25,
-      marginTop:60,
-      marginBottom:60,
-      paddingBottom:5}} onPress={() => {
-        setShowModal(!showModal);}}>
-              <Text style={{fontSize:18,
-        color:"grey",
-        fontWeight:"bold",
-        alignSelf:"center",
-        textTransform:'capitalize',
-        bottom:9}}>Cancel</Text>
+              <TouchableOpacity style={styles.to2} onPress={() => { setShowModal(!showModal);}}>
+              <Text style={styles.txtmodal7}>Cancel</Text>
               </TouchableOpacity>
 
                 <Separator/>
@@ -299,8 +261,12 @@ const styles = StyleSheet.create({
     padding:50,   
     marginTop:410
   },
-  bg1:{backgroundColor:'#000000'},
-  bg2:{backgroundColor:'#ffffff'},
+  bg1:{
+    backgroundColor:'#000000'
+  },
+  bg2:{
+    backgroundColor:'#ffffff'
+  },
   txt1:{
     color:'white',
     textTransform:'capitalize',
@@ -321,6 +287,16 @@ const styles = StyleSheet.create({
     margin: 10, 
     paddingStart: 25, 
     top: -20
+  },
+  txt3:{
+    color: 'black', 
+    textTransform: 'capitalize', 
+    fontSize: 18, 
+    fontWeight: 'bold', 
+    position: 'absolute', 
+    margin: 10, 
+    paddingStart: 25, 
+    top: 75
   },
   txttime1:{
     color: 'grey', 
@@ -406,7 +382,7 @@ txtbottom3:{
   fontSize: 14, 
   fontWeight: 'normal', 
   position: 'absolute',
-  right:248,
+  right:278,
   bottom:40 
 },
 sep1:{
@@ -473,37 +449,182 @@ txtsmall3:{
   left:173
 },
 txtsmall4:{
-
+  textTransform: 'capitalize', 
+  fontSize: 14,
+  color: 'black',
+  fontWeight: 'normal',
+  top:405,
+  position: 'absolute',
+  left:53
 },
 txtsmall5:{
-
+  textTransform: 'capitalize', 
+  fontSize: 14,
+  color: 'black',
+  fontWeight: 'normal',
+  top:405,
+  position: 'absolute',
+  left:113
 },
 txtsmall6:{
-
+  textTransform: 'capitalize', 
+  fontSize: 14,
+  color: 'black',
+  fontWeight: 'normal',
+  top:405,
+  position: 'absolute',
+  left:173
 },
 txtsmall7:{
-
+  textTransform: 'capitalize', 
+  fontSize: 14,
+  color: 'black',
+  fontWeight: 'normal',
+  top:405,
+  position: 'absolute',
+  left:233
 },
 txtsmall8:{
-
+  textTransform: 'capitalize', 
+  fontSize: 14,
+  color: 'black',
+  fontWeight: 'normal',
+  top:444,
+  position: 'absolute',
+  left:53
 },
 txtsmall9:{
-
+  textTransform: 'capitalize', 
+  fontSize: 14,
+  color: 'black',
+  fontWeight: 'normal',
+  top:444,
+  position: 'absolute',
+  left:113
 },
 txtsmall10:{
-
+  textTransform: 'capitalize', 
+  fontSize: 14,
+  color: 'black',
+  fontWeight: 'normal',
+  top:444,
+  position: 'absolute',
+  left:173
 },
 txtsmall11:{
-
+  textTransform: 'capitalize', 
+  fontSize: 14,
+  color: 'black',
+  fontWeight: 'normal',
+  top:444,
+  position: 'absolute',
+  left:233
 },
 txtsmall12:{
-
+  textTransform: 'capitalize', 
+  fontSize: 14,
+  color: 'black',
+  fontWeight: 'normal',
+  top:481,
+  position: 'absolute',
+  left:53
 },
 txtsmall13:{
-
+  textTransform: 'capitalize', 
+  fontSize: 14,
+  color: 'black',
+  fontWeight: 'normal',
+  top:481,
+  position: 'absolute',
+  left:113
 },
 txtsmall14:{
-
+  textTransform: 'capitalize', 
+  fontSize: 14,
+  color: 'black',
+  fontWeight: 'normal',
+  top:481,
+  position: 'absolute',
+  left:173
+},
+txtmodal1:{
+  alignSelf:'center',
+  color:'black',
+  fontSize:18,
+  bottom:-80,
+  fontWeight:'800'
+},
+txtmodal2:{
+  bottom:-120,
+  alignSelf:'center',
+  color:'black', 
+  fontSize:15,
+  fontWeight:'500'
+},
+txtmodal3:{
+  bottom:-120,
+  alignSelf:'center',
+  color:'grey', 
+  fontSize:15
+},
+txtmodal4:{
+  bottom:-120,
+  alignSelf:'center',
+  color:'black', 
+  fontSize:15,
+  left:10,
+  fontWeight:'500'
+},
+txtmodal5:{
+  bottom:-100,
+  alignSelf:'center',
+  color:'grey', 
+  fontSize:15,
+  right:83
+},
+txtmodal6:{
+  fontSize:18,
+  color:"white",
+  fontWeight:"bold",
+  alignSelf:"center",
+  textTransform:'capitalize',
+  bottom:15
+},
+txtmodal7:{
+  fontSize:18,
+  color:"grey",
+  fontWeight:"bold",
+  alignSelf:"center",
+  textTransform:'capitalize',
+  bottom:9
+},
+to1:{
+  elevation:8,
+  backgroundColor:"#f2994a",
+  borderRadius:15,
+  paddingVertical:30,
+  paddingHorizontal:12,
+  margin: 20,
+  width:280,
+  left:20,
+  top:80,
+  marginTop:60,
+  marginBottom:60,
+  paddingBottom:5
+},
+to2:{
+  elevation:8,
+  backgroundColor:"#f0f0f0",
+  borderRadius:15,
+  paddingVertical:20,
+  paddingHorizontal:12,
+  margin: 20,
+  width:280,
+  left:20,
+  top:-25,
+  marginTop:60,
+  marginBottom:60,
+  paddingBottom:5
 },
 view1:{
   alignSelf:'center',
@@ -540,16 +661,180 @@ view3:{
   paddingBottom:30,
   top:-824,
   borderRadius:7,
-  backgroundColor:'rgb(250, 241, 241)'},
+  backgroundColor:'rgb(250, 241, 241)'
+},
+  view4:{
+    alignSelf:'center',
+    height:5,
+    width:50,
+    left:-107,
+    bottom:280,
+    borderWidth:1,
+    borderColor:'grey',
+    paddingBottom:30,
+    top:-785,
+    borderRadius:7
+  },
+  view5:{
+    alignSelf:'center',
+    height:5,
+    width:50,
+    left:-47,
+    bottom:280,
+    borderWidth:1,
+    borderColor:'grey',
+    paddingBottom:30,
+    top:-817,
+    borderRadius:7
+  },
+  view6:{
+    alignSelf:'center',
+    height:5,
+    width:50,
+    left:13,
+    bottom:280,
+    borderWidth:1,
+    borderColor:'grey',
+    paddingBottom:30,
+    top:-849,
+    borderRadius:7
+  },
+  view7:{
+    alignSelf:'center',
+    height:5,
+    width:50,
+    left:73,
+    bottom:280,
+    borderWidth:1,
+    borderColor:'grey',
+    paddingBottom:30,
+    top:-881,
+    borderRadius:7
+  },
+  view8:{
+    alignSelf:'center',
+    height:5,
+    width:50,
+    left:-107,
+    bottom:280,
+    borderWidth:1,
+    borderColor:'grey',
+    paddingBottom:30,
+    top:-875,
+    borderRadius:7
+  },
+  view9:{
+    alignSelf:'center',
+    height:5,
+    width:50,
+    left:-47,
+    bottom:280,
+    borderWidth:1,
+    borderColor:'grey',
+    paddingBottom:30,
+    top:-907,
+    borderRadius:7
+  },
+  view10:{
+    alignSelf:'center',
+    height:5,
+    width:50,
+    left:13,
+    bottom:280,
+    borderWidth:1,
+    borderColor:'grey',
+    paddingBottom:30,
+    top:-939,
+    borderRadius:7
+  },
+  view11:{
+    alignSelf:'center',
+    height:5,
+    width:50,
+    left:73,
+    bottom:280,
+    borderWidth:1,
+    borderColor:'grey',
+    paddingBottom:30,
+    top:-971,
+    borderRadius:7
+  },
+  view12:{
+    alignSelf:'center',
+    height:5,
+    width:50,
+    left:-107,
+    bottom:280,
+    borderWidth:1,
+    borderColor:'grey',
+    paddingBottom:30,
+    top:-965,
+    borderRadius:7
+  },
+  view13:{
+    alignSelf:'center',
+    height:5,
+    width:50,
+    left:-47,
+    bottom:280,
+    borderWidth:1,
+    borderColor:'grey',
+    paddingBottom:30,
+    top:-997,
+    borderRadius:7
+  },
+  view14:{
+    alignSelf:'center',
+    height:5,
+    width:50,
+    left:13,
+    bottom:280,
+    borderWidth:1,
+    borderColor:'grey',
+    paddingBottom:30,
+    top:-1029,
+    borderRadius:7
+  },
   imgclock1:{
-    alignSelf:'flex-start', margin: 10,height:15,width:15,left:25,top:12
+    alignSelf:'flex-start', 
+    margin: 10,
+    height:15,
+    width:15,
+    left:25,
+    top:12
   },
   imgclock2:{
-    alignSelf:'flex-start', margin: 10,height:15,width:15,left:25,top:72
+    alignSelf:'flex-start', 
+    margin: 10,
+    height:15,
+    width:15,
+    left:25,
+    top:72
   },
-
+  imgline1:{
+    position: 'absolute', 
+    margin: 10, 
+    marginStart: 23,
+    top:300,
+    height:10,
+    width:10
+  },
+  imgline2:{
+    position: 'absolute', 
+    marginStart: 26.5,
+    top:319,
+  },
+  imgellipse:{
+    position: 'absolute', 
+    margin: 10, 
+    marginStart: 23,
+    top:368,
+    height:10,
+    width:10
+  },
+  safearea:{
+    flex:0
+  }
 });
-
-
 
 export default Screen9;
